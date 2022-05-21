6ffad6879626a123402c71b48e08433796fc3c42
chengshuping <chengshuping9@163.com>
2022-05-21 21:52:26 +0800

    优化

--- src/components/AsyncComponent/index.tsx
+++ src/components/AsyncComponent/index.tsx
@@ -1,33 +1,34 @@
-import { Result, Spin } from 'antd'
-import React from 'react'
-const MODE = import.meta.env.MODE
-const modules = import.meta.glob('/src/views/**/index.tsx')
-console.log('modules', modules)
-console.log(111)
+import { Result, Spin } from "antd";
+import React from "react";
+const MODE = import.meta.env.MODE;
+const modules = import.meta.glob("/src/views/**/index.tsx");
+console.log("modules", modules);
+console.log(111);
 class AsyncComponent extends React.Component<any> {
   constructor(props: any) {
-    super(props)
+    super(props);
   }
+
   componentDidCatch(error: any, info: any) {
-    if (error && MODE != 'development') {
+    if (error && MODE != "development") {
       // window.location.reload()
     }
-    console.log('error', error)
-    console.log('info', info)
+    console.log("error", error);
+    console.log("info", info);
   }
 
   render() {
-    const { componentName } = this.props
-    const CompontentsImport = modules[`/src/views/${componentName}/index.tsx`]
+    const { componentName } = this.props;
+    const CompontentsImport = modules[`/src/views/${componentName}/index.tsx`];
     if (!CompontentsImport) {
-      console.error(`${componentName} is not found`)
+      console.error(`${componentName} is not found`);
       return (
         <div
           style={{
-            height: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
           <Result
@@ -36,18 +37,18 @@ class AsyncComponent extends React.Component<any> {
             subTitle={`Sorry, ${componentName} is not found`}
           />
         </div>
-      )
+      );
     }
-    const Compontents = React.lazy(CompontentsImport as any)
+    const Compontents = React.lazy(CompontentsImport as any);
     return (
       <React.Suspense
         fallback={
           <div
             style={{
-              height: '100%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
             <Spin tip="页面初始加载中..." />
@@ -56,7 +57,7 @@ class AsyncComponent extends React.Component<any> {
       >
         <Compontents />
       </React.Suspense>
-    )
+    );
   }
 }
-export { AsyncComponent }
+export { AsyncComponent };
