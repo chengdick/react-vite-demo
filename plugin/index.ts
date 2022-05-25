678c9676c23edfe88aa40994ba5a87f5a0b23023
chengshuping <chengshuping9@163.com>
2022-05-25 22:15:03 +0800

    拖拽

--- plugin/index.ts
+++ plugin/index.ts
@@ -16,25 +16,25 @@ export const commonPlugin = (command: string) => {
   let prod: Array<any> = [];
   if (command != "serve") {
     prod.push(
-      viteExternalsPlugin({
-        react: "React",
-        "react-dom": "ReactDOM",
-        axios: "axios",
-      }),
+      // viteExternalsPlugin({
+      //   react: "React",
+      //   "react-dom": "ReactDOM",
+      //   axios: "axios",
+      // }),
       htmlPlugin({
         headScripts: [
           {
             src: "/EasyWasmPlayer.js",
           },
-          {
-            src: "//cdn.jsdelivr.net/npm/react@18.1.0/umd/react.production.min.js",
-          },
-          {
-            src: "//cdn.jsdelivr.net/npm/react-dom@18.1.0/umd/react-dom.production.min.js",
-          },
-          {
-            src: "//cdn.jsdelivr.net/npm/axios@0.27.2/dist/axios.min.js",
-          },
+          // {
+          //   src: "//cdn.jsdelivr.net/npm/react@18.1.0/umd/react.production.min.js",
+          // },
+          // {
+          //   src: "//cdn.jsdelivr.net/npm/react-dom@18.1.0/umd/react-dom.production.min.js",
+          // },
+          // {
+          //   src: "//cdn.jsdelivr.net/npm/axios@0.27.2/dist/axios.min.js",
+          // },
         ],
       })
     );
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,21 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import { Layout } from 'antd'
-import ProLayout, { PageContainer } from '@ant-design/pro-layout'
-import menus from '@/router'
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { Layout } from "antd";
+import ProLayout, { PageContainer } from "@ant-design/pro-layout";
+import canUseDom from "rc-util/lib/Dom/canUseDom";
+import menus from "@/router";
 import {
   Navigate,
   Route,
   Routes,
   useLocation,
   useNavigate,
-} from 'react-router-dom'
-import { AsyncComponent } from '../AsyncComponent'
-const { Header } = Layout
+} from "react-router-dom";
+import { AsyncComponent } from "../AsyncComponent";
+import ReactDOM from "react-dom";
+import "./index.less";
+const { Header } = Layout;
 // 生成路由结构
 const generateRoutes = (menu: any): any => {
   return menu.map((item: any) => {
     if (item.routes && item.routes?.length) {
-      return generateRoutes(item.routes)
+      return generateRoutes(item.routes);
     }
     return (
       <Route
@@ -23,27 +26,86 @@ const generateRoutes = (menu: any): any => {
         path={item.path}
         element={<AsyncComponent componentName={item.component} />}
       ></Route>
-    )
-  })
-}
+    );
+  });
+};
 const RoutesPage: React.FC = () => {
   return (
     <Routes>
       {generateRoutes(menus)}
       <Route path="/*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
-  )
-}
+  );
+};
 
-const defaultProps = { routes: menus }
+const defaultProps = { routes: menus };
 
 export default () => {
-  const location = useLocation()
-  const [pathname, setPathname] = useState<string>()
-  const navigate = useNavigate()
+  const location = useLocation();
+  const [pathname, setPathname] = useState<string>();
+  const navigate = useNavigate();
+  const [width, setWidth] = useState(200);
+  const mousemoveRef = useRef<any>();
   useEffect(() => {
-    setPathname(location.pathname)
-  }, [location.pathname])
+    setPathname(location.pathname);
+  }, [location.pathname]);
+
+  const page = useMemo(() => {
+    return (
+      <PageContainer>
+        <RoutesPage />
+      </PageContainer>
+    );
+  }, []);
+
+  const searchModalContainer = React.useMemo(() => {
+    console.log("searchModalContainer");
+    if (!canUseDom()) {
+      return;
+    }
+    const searchModalContainer$ = document.querySelector(".ant-layout-sider");
+    return searchModalContainer$ || document.body;
+  }, [document.querySelector(".ant-layout-sider")]);
+
+  useEffect(() => {
+    //记录鼠标相对left盒子x轴位置
+    var mouse_x = 0;
+    //设置最大/最小宽度
+    var max_width = 400,
+      min_width = 200;
+    if (document.querySelector(".ant-layout-sider")) {
+      let node = document.createElement("div");
+      node.setAttribute("class", "react-resizable-handle-pro");
+      node.onmousedown = function (e) {
+        var e = e || window.event;
+        //阻止默认事件
+        e.preventDefault();
+        mouse_x = e.clientX - width;
+        document.onmousemove = mouseMove;
+        document.onmouseup = mouseUp;
+        console.log(e, "ll");
+      };
+      document.querySelector(".ant-layout-sider")?.appendChild(node);
+    }
+
+    function mouseMove(e: any) {
+      var e = e || window.event;
+      // var left_width = e.clientX - mouse_x;
+      // left_width = left_width < min_width ? min_width : left_width;
+      // left_width = left_width > max_width ? max_width : left_width;
+      // console.log(left_width, "left_width");
+      // left.style.width = left_width + 'px';
+      // right.style.left = left_width + 'px';
+      setWidth(e.clientX);
+      // console.log(e.clientX);
+    }
+    //终止事件
+    function mouseUp() {
+      document.onmousemove = null;
+      document.onmouseup = null;
+    }
+  }, []);
+
   return (
     <>
       <ProLayout
@@ -53,22 +115,20 @@ export default () => {
         }}
         navTheme="light"
         fixSiderbar
-        headerRender={<Header></Header>}
-        onMenuHeaderClick={(e: any) => console.log(e)}
+        siderWidth={width}
+        headerRender={false}
         menuItemRender={(item: any, dom: any) => (
           <a
             onClick={() => {
-              navigate(item.path || '/dashboard')
+              navigate(item.path || "/dashboard");
             }}
           >
             {dom}
           </a>
         )}
       >
-        <PageContainer>
-          <RoutesPage />
-        </PageContainer>
+        {page}
       </ProLayout>
     </>
-  )
-}
+  );
+};
