1adf82a33402194ddde8966ea6aa1fdbd4e68df6
chengshuping <chengshuping9@163.com>
2022-10-20 17:20:28 +0800

    测试

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import { Layout } from 'antd'
-import ProLayout, { PageContainer } from '@ant-design/pro-layout'
-import menus from '@/router'
+import React, { useEffect, useState } from "react";
+import { Layout } from "antd";
+import ProLayout, { PageContainer } from "@ant-design/pro-layout";
+import menus from "@/router";
 import {
   Navigate,
   Route,
   Routes,
   useLocation,
   useNavigate,
-} from 'react-router-dom'
-import { AsyncComponent } from '../AsyncComponent'
-const { Header } = Layout
+} from "react-router-dom";
+import { AsyncComponent } from "../AsyncComponent";
+const { Header } = Layout;
 // 生成路由结构
 const generateRoutes = (menu: any): any => {
   return menu.map((item: any) => {
     if (item.routes && item.routes?.length) {
-      return generateRoutes(item.routes)
+      return generateRoutes(item.routes);
     }
     return (
       <Route
@@ -23,27 +23,27 @@ const generateRoutes = (menu: any): any => {
         path={item.path}
         element={<AsyncComponent componentName={item.component} />}
       ></Route>
-    )
-  })
-}
+    );
+  });
+};
 const RoutesPage: React.FC = () => {
   return (
     <Routes>
       {generateRoutes(menus)}
       <Route path="/*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
-  )
-}
+  );
+};
 
-const defaultProps = { routes: menus }
+const defaultProps = { routes: menus };
 
 export default () => {
-  const location = useLocation()
-  const [pathname, setPathname] = useState<string>()
-  const navigate = useNavigate()
+  const location = useLocation();
+  const [pathname, setPathname] = useState<string>();
+  const navigate = useNavigate();
   useEffect(() => {
-    setPathname(location.pathname)
-  }, [location.pathname])
+    setPathname(location.pathname);
+  }, [location.pathname]);
   return (
     <>
       <ProLayout
@@ -53,12 +53,12 @@ export default () => {
         }}
         navTheme="light"
         fixSiderbar
-        headerRender={<Header></Header>}
+        // headerRender={<Header></Header>}
         onMenuHeaderClick={(e: any) => console.log(e)}
         menuItemRender={(item: any, dom: any) => (
           <a
             onClick={() => {
-              navigate(item.path || '/dashboard')
+              navigate(item.path || "/dashboard");
             }}
           >
             {dom}
@@ -70,5 +70,5 @@ export default () => {
         </PageContainer>
       </ProLayout>
     </>
-  )
-}
+  );
+};
